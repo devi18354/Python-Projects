58a6bf6a20aba01c5bb0fa1558b92b0f4dfcf7ac
Devi Surya Prasanna Nulu <devinulu6@gmail.com>
2026-04-11 17:08:49 +0530

    Create mainproject.py

---
+++ mainproject.py
@@ -0,0 +1,9 @@
+# My First Python Project
+print("Hello GitHub! This is my first Python project.")
+
+# Simple Logic: Checking if a number is Even or Odd
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print(f"{num} is an Even number.")
+else:
+    print(f"{num} is an Odd number.")
