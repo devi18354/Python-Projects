1e0a934e8e5ed973e302e8f50494e4a4fc174c90
Devi Surya Prasanna Nulu <devinulu6@gmail.com>
2026-04-13 11:50:39 +0530

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,12 @@
+# Simple Calculator
+print("--- Simple Calculator ---")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print(f"Addition: {num1 + num2}")
+print(f"Subtraction: {num1 - num2}")
+print(f"Multiplication: {num1 * num2}")
+if num2 != 0:
+    print(f"Division: {num1 / num2}")
+else:
+    print("Cannot divide by zero")
